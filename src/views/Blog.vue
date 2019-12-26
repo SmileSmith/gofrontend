584b3b0b4bf7ba37fb7073ac95cfce54b24e5f13
SmileSmith <longde_chen@163.com>
2019-12-26 18:05:09 +0800

    refactor: 放弃内部组件的形式&微前端，改用iframe解耦两个工程

---
+++ src/views/Blog.vue
@@ -0,0 +1,11 @@
+<template>
+  <iframe></iframe>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,11 @@
 <template>
-  <test />
+  <iframe></iframe>
 </template>
 
 <script>
-import test from './../docs/test.md';
-
 export default {
   name: 'Home',
-
-  components: {
-    test,
-  },
 };
 </script>
+
+<style></style>
