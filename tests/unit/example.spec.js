a417dcb20871b13ab0af909d4246fa91f0958815
SmileSmith <longde_chen@163.com>
2019-12-29 12:40:19 +0800

    fix: 修复单元测试例子

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,14 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { shallowMount } from '@vue/test-utils';
+import About from '@/views/About.vue';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import store from '@/store';
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
+Vue.use(Vuex);
+
+describe('About.vue', () => {
+  it('text: This is an about page', () => {
+    const AboutComponent = shallowMount(About, { store });
+    expect(AboutComponent.text()).toEqual('This is an about page');
   });
 });
